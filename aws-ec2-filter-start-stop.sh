04de486d1be55f17cb7b6bcea10ad0a7d87a3b52
Ramalingam <magnilamars@gmail.com>
2020-07-31 16:36:30 +0530

    bash script for filter and terminate the aws ec2 instance

--- aws-ec2-filter-start-stop.sh
+++ aws-ec2-filter-start-stop.sh
@@ -1,34 +1,16 @@
+#Disable-Termination-Protection
 #!/bin/bash
-echo "server_type: $1"
-echo "tenant_number1: $2"
-echo "tenant_number2: $3"
-echo "Servers are" $1-$2 "and" $1-$3 
-for instance in $(aws ec2 describe-instances --filters 'Name=tag:Name,Values='$1-$2','$1-$3'' | grep InstanceId | cut -d '"' -f 4 | sort); do
-    echo $instance
+#!/usr/bin/env bash
+echo "enter the server_name"
+read server_name
+echo "enter first tenant_number"
+read tenant_number1
+echo "enter second tenant_number"
+read tenant_number2
+for instance in $(aws ec2 describe-instances --filters 'Name=tag:Name,Values='$server_name-$tenant_number1','$server_name-$tenant_number2'' | grep InstanceId | cut -d '"' -f 4 | sort); do 
     aws ec2 describe-instance-attribute --instance-id $instance --attribute disableApiTermination | grep Value | cut -d : -f 2
-    #aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].[State.Name]' --output text
-    #aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].[InstanceId]' --output text
+    aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $instance
+    # aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].[State.Name]' --output text
 done
-exit
-
-# **************************************************************************
-# for instance in $(aws ec2 describe-instances --filters 'Name=tag:Name,Values=ppv-stg-tenant2-rdp01-srv,ppv-stg-tenant2-svc01-srv' | grep InstanceId | cut -d '"' -f 4 | sort); do
-#     echo -n $instance
-#     aws ec2 describe-instance-attribute --instance-id $instance --attribute disableApiTermination | grep Value | cut -d : -f 2
-#     aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].[State.Name]'
-# done
-
-# aws ec2 describe-instances --filters Name=tag:Environment,Values=stg-tenant2 --query "Reservations[].Instances[].{Instance:InstanceId,Name:Tags[?Key=='Name']|[0].Value}" --output table
-# aws ec2 describe-instances --filters Name=tag:Environment,Values=stg-tenant2 --query 'Reservations[].Instances[].[InstanceId]' --output text
-# aws ec2 describe-instances --filters Name=tag:Environment,Values=stg-tenant2 --query "Reservations[].Instances[].{Instance:InstanceId,Name:Tags[?Key=='Name']|[0].Value}" --output table | grep -E "svc|web|mgmt|jenkins|elasticsearch"
-
-# if you run like this :
-# Name=tag:Environment,Values=stg-tenant2
-# You get 8 instance 
-# Out of that, I need 4 instanceid 
-
-# Requirement 
-# 8 box name 
-# 4 box  = only instance id - stop
-# aws ec2 describe-instances --filters 'Name=tag:Name,Values=Webserver-1' | grep InstanceId | cut -d '"' -f 4 | sort
-# aws ec2 describe-instance-attribute --instance-id i-0568507be7a8c011b --attribute disableApiTermination | grep Value | cut -d : -f 2
+echo $instance
+echo "Termination protection is successfully disabled"
