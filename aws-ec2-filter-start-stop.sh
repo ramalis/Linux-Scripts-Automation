c0ea9456231a068687d83a4d42bbd401f86564d5
Ramalingam <magnilamars@gmail.com>
2020-07-31 17:37:20 +0530

    bash script for filter and terminate the aws ec2 instance

--- aws-ec2-filter-start-stop.sh
+++ aws-ec2-filter-start-stop.sh
@@ -1,6 +1,5 @@
 #Disable-Termination-Protection
 #!/bin/bash
-#!/usr/bin/env bash
 echo "enter the server_name"
 read server_name
 echo "enter first tenant_number"
@@ -10,7 +9,7 @@ read tenant_number2
 for instance in $(aws ec2 describe-instances --filters 'Name=tag:Name,Values='$server_name-$tenant_number1','$server_name-$tenant_number2'' | grep InstanceId | cut -d '"' -f 4 | sort); do 
     echo $instance
     aws ec2 describe-instance-attribute --instance-id $instance --attribute disableApiTermination | grep Value | cut -d : -f 2
-    aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $instance
-    # aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].[State.Name]' --output text
+    aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $instance | grep Value | cut -d : -f 2
+    # aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].[State.Name]' --output table | grep Value | cut -d : -f 2
 done
 echo "Termination protection is successfully disabled"
